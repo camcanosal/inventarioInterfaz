b3ae877df8a8127df8cd0b5c45d9d46c0d5386ab
Cano <camilo.cano@softwareone.com>
2024-10-09 12:01:32 -0500

    Inicializar frontend

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom(HttpClientModule),  // Asegúrate de importar HttpClientModule aquí
+    provideRouter(routes),
+    provideClientHydration(),
+    FormsModule
+  ]
 };
